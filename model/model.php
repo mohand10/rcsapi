7460b85f064d6853022a0c9ed6c8fa7d453357f8
monty <73382670+mohand10@users.noreply.github.com>
2021-12-17 13:43:44 +0530

    Update model.php

--- model/model.php
+++ model/model.php
@@ -5,9 +5,10 @@ require __DIR__ . './../utils/util.php';
 require __DIR__ . './../include/Command.php';
 require __DIR__ . './../include/AddCommand.php';
 require __DIR__ . './../include/DeleteCommand.php';
+require __DIR__ . './../include/EditCommand.php';
 MySQL::connect();
 class RcsModel implements RcsApiService
-{
+{    //const $IPAddress = $_SERVER['REMOTE_ADDR'];
 
     public static function getPhoneByPhoneID($id)
     {
@@ -37,7 +38,7 @@ class RcsModel implements RcsApiService
         if (MySQL::getAffectedRows() >= 1) {
             $resArr["status code"]   = 200;
             $resArr["phone details"] = $arrRow;
-            return json_encode($resArr);
+            return $resArr;
         } else {
             throw new Exception("No phone details found - check with the PhoneID", 404);
         }
@@ -59,6 +60,7 @@ class RcsModel implements RcsApiService
         $arrRow = array();
         while ($row = MySQL::getNextRow()) {
             $arrRow[] = array(
+                "PhoneID" => $row["PID"],
                 "ServerName" => iconv("ISO-8859-1", "UTF-8", $row["ServerName"]),
                 "BrandingName" => iconv("ISO-8859-1", "UTF-8", $row["BrandingName"]),
                 "VendorName" => iconv("ISO-8859-1", "UTF-8", $row["VendorName"]),
@@ -72,7 +74,7 @@ class RcsModel implements RcsApiService
         if (MySQL::getAffectedRows() >= 1) {
             $resArr["status code"] = 200;
             $resArr["phone details"] = $arrRow;
-            return json_encode($resArr);
+            return $resArr;
         } else {
             throw new Exception("No phone(s) details found", 404);
         }
@@ -107,7 +109,7 @@ class RcsModel implements RcsApiService
         if (MySQL::getAffectedRows() >= 1) {
             $resArr["status code"] = 200;
             $resArr["phone details"] = $arrRow;
-            return json_encode($resArr);
+            return $resArr;
         } else {
             throw new Exception("No phone details found - check with the ServerID", 404);
         }
@@ -148,7 +150,6 @@ class RcsModel implements RcsApiService
                 $tlab = $row["Label"];
                 $tval = $row["Count"];
                 $counts[$row["Label"]] = $row["Count"];
-                // error_log("\n Row 3 Data $tlab = $tval \n",3,"/var/tmp/my-errors.log");            
             }
         } else {
             foreach ($tables as $t) {
@@ -173,27 +174,25 @@ class RcsModel implements RcsApiService
             $summary["Servers"] = $counts["Server"];
             $summary["Phones"] = $counts["Phone"];
 
-            //$numunaff       = $counts["Unaffiliated"] + 0;        // Add + 0 to ensure there's a number there
-            //$numunreg       = $counts["Unregistered"] + 0;        // (it's possible these are null)
-            $numbrands      = $counts["Branding"] + 0;
-            $nummaxbrands   = $counts["MaxBrandings"] + 0;
+            //$numbrands      = $counts["Branding"] + 0;
+            //$nummaxbrands   = $counts["MaxBrandings"] + 0;
 
-            $summary["Branding Sets"] = $numbrands . " used out of " . $nummaxbrands . " available";
+            $summary["Branding Sets"] = $counts["Branding"] . " used out of " . $counts["MaxBrandings"] . " available";
             $summary["Default Branding"] = $defaultbranding;
             $summary["Default Firmware"] = $defaultfirmware;
 
             $vendorNameQuery = "SELECT Vendor.Name as vendorName FROM Vendor WHERE VendorID=$vid";
             MySQL::runQuery($vendorNameQuery);
             $row = MySQL::getNextRow();
-            $resArr["Account summary for vendor - " . $row["vendorName"]] = $summary;
             $resArr["status code"] = 200;
-            return json_encode($resArr);
+            $resArr["Account summary for vendor - " . $row["vendorName"]] = $summary;
+            return $resArr;
         } else {
             throw new Exception("Details Not Found - check with the VendorID", 404);
         }
     }
 
-    public static function deleteByPhoneId($data)
+    public static function deletePhone($data)
     {
         $data['phonemaclist'] = (isset($data['phonemaclist'])) ? $data['phonemaclist'] : "NULL";
         if ($data['phonemaclist'] == "") {
@@ -212,16 +211,13 @@ class RcsModel implements RcsApiService
                     $num++;
                 }
             }
-            $resArr["message"] = ($num . (($num == 1) ? " phone has" : " phones have") . " been deleted.");
-            return json_encode($resArr);
         }
+        $resArr["message"] = ($num . (($num == 1) ? " phone has" : " phones have") . " been deleted.");
+        return $resArr;
     }
-
-
     //add phones
     public static function addPhone($data)
     {
-
         if (isset($data["VendorID"])) {
             $vendorid = $data["VendorID"];
         } else {
@@ -230,7 +226,7 @@ class RcsModel implements RcsApiService
         $serverid = (isset($data["ServerID"])) ? $data["ServerID"] : "NULL";
         $branding = (isset($data["BrandingID"])) ? $data["BrandingID"] : "NULL";;
         $firmware = (isset($data["FirmwareID"])) ? $data["FirmwareID"] : "NULL";
-        //$IPAddress = (isset($data["IPAddress"])) ? $data["IPAddress"] : "NULL";
+
 
         $data['addphonemaclist'] = (isset($data['addphonemaclist'])) ? $data['addphonemaclist'] : "NULL";
 
@@ -243,15 +239,12 @@ class RcsModel implements RcsApiService
         } else {
             $errorCount = 0;
             $num = 0;
-            //$macs = explode(";", rtrim($_POST['addphonemaclist_string'], ";"));
             $macs = explode(" ", $data['addphonemaclist']);
             foreach ($macs as $mac) {
                 $mac = util::cleanMacAddress($mac);
             }
             foreach ($macs as $mac) {
-                //$mac = Globals::cleanMacAddress($mac);
                 if ($mac !== false) {
-
                     $existsQuery = "SELECT * FROM Phone WHERE PhoneID = '" . hexdec($mac) . "'";
                     MySQL::runQuery($existsQuery);
 
@@ -264,6 +257,7 @@ class RcsModel implements RcsApiService
                         $command->setParam("ServerID", $serverid);
                         $command->setParam("BrandingID", $branding);
                         $command->setParam("FirmwareID", $firmware);
+                        $command->setParam("IPAddress", $_SERVER['REMOTE_ADDR']);
                         //$command->setParam("AddedBy", $userid);
                         $command->setParam("DateAdded", MySQL::getCurrentDate());
                         try {
@@ -279,45 +273,70 @@ class RcsModel implements RcsApiService
                 }
             }
             if ($num > 0) {
-                $resArr["message"] = $num . (($num == 1) ? ' phone has' : ' phones have') . ' been added or updated';
                 $resArr["status code"] = 201;
-                return json_encode($resArr);
+                $resArr["message"] = $num . (($num == 1) ? ' phone has' : ' phones have') . ' been added or updated';
+                return $resArr;
             } else {
                 throw new Exception("Error adding $mac to database", 409);
             }
-           
         }
     }
 
-    public static function updatePhonesByPhoneId($pid, $data)
+    public static function updatePhone($data)
     {
-        $query = "SELECT * FROM Phone WHERE PhoneID =$pid";
-        MYSQL::runQuery($query);
-        $row = MySQL::getNextRow();
-        $VendorID = (isset($data["VendorID"])) ? $data["VendorID"] : $row["vendorID"];
-        $ServerID = (isset($data["ServerID"])) ? $data["ServerID"] : $row["ServerID"];
-        $BrandingID = (isset($data["BrandingID"])) ? $data["BrandingID"] : $row["BrandingID"];
-        $FirmwareID = (isset($data["FirmwareID"])) ? $data["FirmwareID"] : $row["FirmwareID"];
-        $IPAddress = (isset($data["IPAddress"])) ? $data["IPAddress"] : $row["IPAddress"];
-
-        $query = "UPDATE Phone SET ServerID=$ServerID, VendorID=$VendorID,ServerID=$ServerID,
-                        BrandingID=$BrandingID,FirmwareID=$FirmwareID,IPAddress=$IPAddress WHERE PhoneID=$pid";
-        $res = MySQL::runQuery($query, $totalrows = null);
-
-        if (MySQL::getAffectedRows() > 0) {
-            $resArr["message"] = "phone updated";
-            $resArr["status code"] = 200;
-            return json_encode($resArr);
+        $data['phonemaclist'] = (isset($data['phonemaclist'])) ? $data['phonemaclist'] : "NULL";
+        if ($data['phonemaclist'] == "") {
+            throw new Exception("No Phones updated.", 409);
         } else {
-            throw new Exception("Phone not updated - check with the PhoneID or try changing input data", 409);
+            $macs = explode(" ", $data['phonemaclist']);
+            foreach ($macs as $mac) {
+                $mac = util::cleanMacAddress($mac);
+            }
+            $num = 0;
+            foreach ($macs as $mac) {
+                $query = "SELECT * FROM Phone WHERE PhoneID = 0x$mac";
+                MySQL::runQuery($query);
+                $row = MySQL::getNextRow();
+                //$VendorID = (isset($data["VendorID"])) ? $data["VendorID"] : $row["vendorID"];
+                $ServerID = (isset($data["ServerID"])) ? $data["ServerID"] : $row["ServerID"];
+                $BrandingID = (isset($data["BrandingID"])) ? $data["BrandingID"] : $row["BrandingID"];
+                $FirmwareID = (isset($data["FirmwareID"])) ? $data["FirmwareID"] : $row["FirmwareID"];
+                //$IPAddress = (isset($data["IPAddress"])) ? $data["IPAddress"] : $row["IPAddress"];
+
+                if ($mac !== false) {
+                    $command = new EditCommand("Phone");
+                    $command->setID(hexdec($mac));
+                    $command->setParam("ServerID", $ServerID);
+                    $command->setParam("BrandingID", $BrandingID);
+                    $command->setParam("FirmwareID", $FirmwareID);
+                    $command->setParam("IPAddress", $_SERVER['REMOTE_ADDR']);
+                    //$command->setParam("ReferenceID", $_POST['referenceID']);
+                    $execute = $command->execute();
+                    if ($execute) {
+                        $num++;
+                    }
+                }
+            }
+            $resArr["status code"] = 200;
+            if ($num == 0) {
+                $resArr["message"] = "No phones have been modified.";
+            } else if ($num == 1) {
+                $resArr["message"] = "1 phone has been modified.";
+            } else {
+                $resArr["message"] = "$num phones have been modified.";
+            }
         }
+        return $resArr;
     }
 
+
+    //jwt token generation
     public static function getToken($pid)
     {
-        $now = new DateTime();
-        $future = new DateTime('+30 min');
-        $secretKey = "secret_key";
+        //$now = new DateTime();
+        $now = time();
+        $future = strtotime('+30 min', $now);
+        $secretKey = "Rcs@api123";
         $payload = [
             "jti" => $pid,
             "iat" => $now,
@@ -336,7 +355,74 @@ class RcsModel implements RcsApiService
         $jwt['now'] = $now;
         $jwt['future'] = $future;
         //$jwt["Expires in"] = (($future-$now)/60)." minutes";
+        return $jwt;
+    }
 
-        return json_encode($jwt);
+    //private $user_name='';
+    public static function login($data)
+    {
+        $data['username'] = isset($data['username']) ? $data['username'] : '';
+        $data['password'] = isset($data['password']) ? $data['password'] : '';
+        //$dat = RcsModel::getToken();
+
+        MySQL::connect();
+        $un = MySQL::cleanString($data['username']);
+        $this->user_name = $un;
+        $pw = md5(MySQL::cleanString($data['password']));
+        $raw_pwd = MySQL::cleanString($data['password']);
+        $reverseSalt = "$" . strrev(explode("@", $un)[0]) . "$";
+        $pw256 = hash('sha256', $raw_pwd . $reverseSalt);
+        $q = "SELECT User.UserID, User.VendorID, User.Username, User.Name, User.Rights, User.Eula, Vendor.Name AS VendorName, Vendor.NumBrandings, Vendor.CanFirmware " .
+            ",DATEDIFF(CURDATE(),User.lastModified) as expiryDays, User.loginAttempts,TIMESTAMPDIFF(MINUTE,lastLoginTime,CURRENT_TIMESTAMP()) as locked ,isMD5     " . // Added new columns
+            "FROM User LEFT JOIN Vendor ON (Vendor.VendorID=User.VendorID) WHERE Username='$un' AND Password in ('$pw','$pw256')";
+
+        MySQL::runQuery($q);
+        if (MySQL::getAffectedRows() == 1) {
+
+            $row = MySQL::getNextRow();
+            //return json_encode($row);
+            $userid =  $row['UserID'];
+            if ($row['loginAttempts'] >= 3) {
+                if ($row['locked'] > 15) {
+                    $updateUser = "Update User set loginAttempts=0,lastLoginTime=CURRENT_TIMESTAMP() where Username='" . $un . "'";
+                    MySQL::runQuery($updateUser);
+                } else if ($row['locked'] <= 15) {
+                    $value = 15 - $row['locked'];
+                    if ($value >= 15) {
+                        $value = 15;
+                    }
+                    if ($value == 0) {
+                        $value = 1;
+                    }
+                    throw new Exception("Account is Locked!! <br/> Please try after " . ($value) . " minutes.", 404);
+                }
+            }
+
+            $_SESSION['userid'] = $userid;
+            $_SESSION['username'] = $un;
+            $_SESSION["login_time_stamp"] = time();
+
+            if ($row['expiryDays'] >= 45) {
+                if ($row['isMD5'] == 0) {
+                    $updateUser = "Update User set loginAttempts=0,lastLoginTime=CURRENT_TIMESTAMP(),password ='" . $pw256 . "' where Username='" . $un . "'";
+                    MySQL::runQuery($updateUser);
+                } elseif ($row['isMD5'] == 1) {
+                    $updateUser = "Update User set loginAttempts=0,lastLoginTime=CURRENT_TIMESTAMP() where Username='" . $un . "'";
+                    MySQL::runQuery($updateUser);
+                }
+                $resArr['message'] = "Password expired, please reset it.";
+                $updateUser = "Update User set loginAttempts=0,lastLoginTime=CURRENT_TIMESTAMP() where Username='" . $un . "'";
+                MySQL::runQuery($updateUser);
+            } else {
+                $query = "INSERT INTO LoginAudits (UserID, ActionName, ActionTime) VALUES('$userid','Successfully Login',CURRENT_TIMESTAMP())";
+                MySQL::runQuery($query);
+                $resArr['status code'] = 200;
+                $resArr['message'] = "Successful login & password expires in " . (45 - $row['expiryDays']) . " days";
+                $resArr['token'] = (self::getToken($un));
+            }
+        } else {
+            throw new Exception("Invalid credentials", 404);
+        }
+        return $resArr;
     }
 }
